c6aa504531fe80501c64a6550e03dfd280478415
Paulo Rafael <paulorafaelcarvalhorodrigues@gmail.com>
2024-05-15 14:42:13 -0300

    App criado em typeScript

--- App.tsx
+++ App.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import  ButtomProximo  from './src/components/buttons/buttonProximo';
-import ButtomCausasDiagnostico from './src/components/buttons/buttonCausasDiagnostico';
+
 import Home from './src/pages/TelaInicial';
 
 export default function App() {
